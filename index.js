a0a89aea7da2c8472a54df0b907d3ec6ebf516b2
sophiemaniscalco <sophmaniscalco@gmail.com>
2018-06-21 21:12:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 //Code your solution here
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget(target) {
+  return document.querySelector("#nested div.target")
+}
+
+function deepestChild(grand-node){
+  return document.querySelector("div#grand-node")
+}
+
