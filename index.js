11f76ca1b83a4ec89879e5b9ca327555899f194a
sophiemaniscalco <sophmaniscalco@gmail.com>
2018-06-21 21:19:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ function deepestChild(){
   return document.querySelector("#grand-node div div div div")
 }
 
-increaseRankBy(n) {
-  
+function increaseRankBy(n) {
+const rank = document.querySelectorAll('ul.ranked-list')
+
+for(let i = 0; i < rank.length; i++) {
+  rank[i].innerHTML = (i + 1).toString()
 }
