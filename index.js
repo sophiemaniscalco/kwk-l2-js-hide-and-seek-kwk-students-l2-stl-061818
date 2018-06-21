52c883800c587a454c0d163b99f995ee3333f9ae
sophiemaniscalco <sophmaniscalco@gmail.com>
2018-06-21 21:19:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 //Code your solution here
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget(target) {
+  return document.querySelector("#nested div.target")
+}
+
+function deepestChild(){
+  return document.querySelector("#grand-node div div div div")
+}
+
+function increaseRankBy(n) {
+const rank = document.querySelectorAll('ul.ranked-list')
+
+for(let i = 0; i < rank.length; i++) {
+  rank[i].innerHTML = (i + 1).toString()
+}
+}
